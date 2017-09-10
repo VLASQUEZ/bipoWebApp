cec3cbac261dfd32c59d354672ade99a40a98992
Andres <alvaro.velasquez@olimpiait.com>
2017-09-10 18:56:44 -0500

    ngcookies

--- page/js/controllers.js
+++ page/js/controllers.js
@@ -276,7 +276,7 @@ angular.module('bipoApp.controllers', ['ngAnimate', 'ngSanitize','ui.bootstrap']
     	}
     }
 
-}).controller('HeatMapCtrl',['NgMap','heatMapResource','$scope',function (NgMap,heatMapResource,$scope) {
+}).controller('HeatMapCtrl',function (NgMap,heatMapResource,$scope) {
     console.log('Mapa');
     $scope.taxiData = [
         new google.maps.LatLng(4.651006, -74.066541),
@@ -335,4 +335,4 @@ angular.module('bipoApp.controllers', ['ngAnimate', 'ngSanitize','ui.bootstrap']
     vm.changeOpacity = function() {
         heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
     }
-}]);
+});
