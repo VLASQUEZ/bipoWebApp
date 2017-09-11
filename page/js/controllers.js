a4f3ec201178eb6f95b5221ae0a062528a9bb090
Miguel Angel Tovar <mtovar@progracol.com>
2017-09-10 19:36:01 -0500

    Ajuste Coordenadas de Servicio

--- page/js/controllers.js
+++ page/js/controllers.js
@@ -297,46 +297,45 @@ angular.module('bipoApp.controllers', ['ngAnimate', 'ngSanitize','ui.bootstrap']
         	angular.forEach(data.reports,function (value, key) {
 				$scope.taxiData.push(new google.maps.LatLng(value.latitude, value.longitude));
             });
+            var heatmap, vm = this;
+            vm.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZm14lpvD7-Pahl6cCSwIXAlquw1p46-U"
 
+            NgMap.getMap().then(function(map) {
+                vm.map = map;
+                heatmap = vm.map.heatmapLayers.foo;
+            });
+            vm.toggleHeatmap= function(event) {
+                heatmap.setMap(heatmap.getMap() ? null : vm.map);
+                vm.changeGradient();
+            };
+            vm.changeGradient = function() {
+                var gradient = [
+                    'rgba(0, 255, 255, 0)',
+                    'rgba(0, 255, 255, 1)',
+                    'rgba(0, 191, 255, 1)',
+                    'rgba(0, 127, 255, 1)',
+                    'rgba(0, 63, 255, 1)',
+                    'rgba(0, 0, 255, 1)',
+                    'rgba(0, 0, 223, 1)',
+                    'rgba(0, 0, 191, 1)',
+                    'rgba(0, 0, 159, 1)',
+                    'rgba(0, 0, 127, 1)',
+                    'rgba(63, 0, 91, 1)',
+                    'rgba(127, 0, 63, 1)',
+                    'rgba(191, 0, 31, 1)',
+                    'rgba(255, 0, 0, 1)'
+                ]
+                heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+            }
+            vm.changeRadius = function() {
+                heatmap.set('radius', heatmap.get('radius') ? null : 20);
+            }
+            vm.changeOpacity = function() {
+                heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+            }
         	//$scope.bikeTypes=data.biketypes;
         });
     /*heatMapResource.query(function (completed, headers) {
         console.log(completed);
     })*/
-    var heatmap, vm = this;
-    vm.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZm14lpvD7-Pahl6cCSwIXAlquw1p46-U"
-
-    NgMap.getMap().then(function(map) {
-        vm.map = map;
-        heatmap = vm.map.heatmapLayers.foo;
-    });
-    vm.toggleHeatmap= function(event) {
-        heatmap.setMap(heatmap.getMap() ? null : vm.map);
-        vm.changeGradient();
-    };
-    vm.changeGradient = function() {
-        var gradient = [
-            'rgba(0, 255, 255, 0)',
-            'rgba(0, 255, 255, 1)',
-            'rgba(0, 191, 255, 1)',
-            'rgba(0, 127, 255, 1)',
-            'rgba(0, 63, 255, 1)',
-            'rgba(0, 0, 255, 1)',
-            'rgba(0, 0, 223, 1)',
-            'rgba(0, 0, 191, 1)',
-            'rgba(0, 0, 159, 1)',
-            'rgba(0, 0, 127, 1)',
-            'rgba(63, 0, 91, 1)',
-            'rgba(127, 0, 63, 1)',
-            'rgba(191, 0, 31, 1)',
-            'rgba(255, 0, 0, 1)'
-        ]
-        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-    }
-    vm.changeRadius = function() {
-        heatmap.set('radius', heatmap.get('radius') ? null : 20);
-    }
-    vm.changeOpacity = function() {
-        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-    }
 });
